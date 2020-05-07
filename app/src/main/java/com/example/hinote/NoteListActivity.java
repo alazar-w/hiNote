4f8d0cb952ba8c05efaf7869e268dec1715a448f
alazar-w <ej.dalvic@gmail.com>
2020-05-08 00:09:57 +0300

    creating note and listing note with testing done!!

---
+++ app/src/main/java/com/example/hinote/NoteListActivity.java
@@ -0,0 +1,78 @@
+package com.example.hinote;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+public class NoteListActivity extends AppCompatActivity {
+
+    private ArrayAdapter<NoteInfo> mAdapterNotes;
+
+    //only called when our activity is first created
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NoteListActivity.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        //since onCreate is  only called once we need to let notify our adapter
+        //in initializeDisplayContent() if we change any thing when we resume back
+        initializeDisplayContent();
+
+
+    }
+
+    //whenever we get back to the NoteListActivity we need to notify the ArrayAdapter that if any data is changed
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAdapterNotes.notifyDataSetChanged();
+
+    }
+
+    private void initializeDisplayContent() {
+        //final it for our listNotes to be acessable in the anonymous class below
+        final ListView listNotes = findViewById(R.id.list_notes);
+
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        mAdapterNotes = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,notes);
+        listNotes.setAdapter(mAdapterNotes);
+
+        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> listNotes, View view, int position, long id) {
+                Intent intent = new Intent(NoteListActivity.this,MainActivity.class);
+                //here we can send the noteInfo in the intent to mainActivity,but we can also just send the position and main
+                //activity can get the notInfo like the method we wrote below.
+                //NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(position);
+
+                intent.putExtra(MainActivity.NOTE_POSITION,position);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+
+}
